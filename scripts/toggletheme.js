c65e5b83d67bc3b17ec63f1c45794ccdd87f3b3f
horribleprogram <horribleprogrammer.email@gmail.com>
2021-02-12 14:47:42 -0500

    Skeleton template

---
+++ scripts/toggletheme.js
@@ -0,0 +1,4 @@
+const userTheme = document.getElementById('link-user-theme');
+const lightTheme = document.getElementById('link-light-theme');
+const darkTheme = document.getElementById('link-dark-theme');
+
